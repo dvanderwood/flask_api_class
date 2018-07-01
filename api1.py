3ee7263721387912fe94420c5926d4bf5dadb810
Drew Vander Wood <dvanderwood@gmail.com>
2018-07-01 15:21:07 -0400

    first api lesson

---
+++ api1.py
@@ -0,0 +1,49 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+stores =[
+	{
+		'name': 'My Wonderful Store',
+		'items': [
+			{
+			'name': 'My Item',
+			'price': 15.99
+			}
+		]
+	}
+]
+
+# POST - used to reveive data
+# GET - used to send data back only
+
+
+
+# POST /store data: {name}
+@app.route('/store', methods = ['POST'])
+def create_store():
+	pass
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>', methods = ['GET'])
+def get_store(name):
+	pass
+
+# GET /store
+@app.route('/store', methods = ['GET'])
+def get_stores():
+	pass
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods = ['POST'])
+def create_item_in_store(name):
+	pass
+
+# GET store/<string:name>/item
+@app.route('/store/<string:name>/item', methods = ['GET'])
+def get_items_in_store(name):
+	pass
+
+
+
+app.run(port = 5000, debug = True)
